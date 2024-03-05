d91e63ff2b2a672ae646625a45b01e5ffbb10565
priyam125 <sinha.priyam002@gmail.com>
2024-03-06 04:12:21 +0530

    font-added

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1
-        className="text-white text-center"
-        style={{ color: "white", textAlign: "center" }}
-      >
-        Foodverse
-      </h1>
+      <h1 className="text-white text-center p-4">Foodverse</h1>
     </main>
   );
 }
